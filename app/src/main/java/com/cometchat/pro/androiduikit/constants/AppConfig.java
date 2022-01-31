964070ffe7dc401f7ca4625bf41340cb2f5db34e
seri <rohan5859>
2022-01-31 20:42:18 +0530

    Initial commit

---
+++ app/src/main/java/com/cometchat/pro/androiduikit/constants/AppConfig.java
@@ -0,0 +1,14 @@
+package com.cometchat.pro.androiduikit.constants;
+
+public class AppConfig {
+
+    public class AppDetails {
+
+        public static final String APP_ID = "XXXXXXXXXXXXXXXX";
+
+        public static final String AUTH_KEY = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+
+        public static final String REGION = "XX";
+    }
+}
+
